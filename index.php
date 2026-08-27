c28a317099066e3224565632b8b30fc6c821d234
Libor Mitrenga <libor@mitrengovi.cz>
2026-08-27 21:24:09 +0200

    Podpora PWA

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Gallery</title>
+<meta name="theme-color" content="#0F5FA8">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="icon" href="favicon.ico" sizes="any">
+<link rel="icon" href="images/app-icon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" sizes="192x192" href="images/app-icon-192x192.png">
+<link rel="manifest" href="manifest.webmanifest">
 <link rel="stylesheet" href="style.css?v=<?= filemtime(__DIR__ . '/style.css') ?>">
 </head>
 <body>
